8d2b55d34a622a55cbebb62a0eaf1bdc4342c05d
apendo-emil <emil.brolin@apendo.se>
2022-06-29 12:47:52 +0200

    Updated server

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    console.log('Server called');
+    res.send('Hello World!');
+})
 
-app.listen(port, () => console.log(`My server listening at http://localhost:${port}`))
+app.listen(port, function() {
+    console.log('Running on port ' + port);
+})
